2a84631acb55c83823a75eab237527701a3b9c14
Gerard_Castro-Linares <g.castrolinares@tudelft.nl>
2019-11-06 12:30:56 +0100

    final changes

--- final.py
+++ final.py
@@ -1,5 +1,5 @@
 import script
 
 print('circumference', script.circumference(5))
-
+print('surface area', script.area_circle(5))
 print("pero zi ezto eh lo mimo' que ante'")
--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 import math
 
-def circumference(r):
+def circumference(r): 
+	""" Function that computes the circumference of a circle of radius r
+            The radius parameter has to be given"""
     return 2*math.pi*r
 
 def area_circle(r):
+    """ Function that computes the surface area of a circle of radius r
+            The radius parameter has to be given"""
     return math.pi*r**2
+
